c9ea3b218a8e6831677220b85f69900662a1cf63
hangjiaz <zhaohj314@gmail.com>
2023-10-31 10:35:33 +0100

    move num bins to internal data

--- R/data.R
+++ R/data.R
@@ -4,24 +4,3 @@
 #' @return segmented data
 #'
 "example.seg"
-#'
-#' A vector that stores the number of 1MB-size genomic bins for each chromosome from the hg18 genome. 
-#' It is used to normalize the segment lengths to the corresponding chromosomes.  
-#'
-#' @return the numebr of genomic bins in the hg18 genome
-#'
-"hg18_chr_bins"
-#'
-#' A vector that stores the number of 1MB-size genomic bins for each chromosome from the hg19 genome. 
-#' It is used to normalize the segment lengths to the corresponding chromosomes.  
-#'
-#' @return the numebr of genomic bins in the hg19 genome
-#'
-"hg19_chr_bins"
-#'
-#' A vector that stores the number of 1MB-size genomic bins for each chromosome from the hg38 genome. 
-#' It is used to normalize the segment lengths to the corresponding chromosomes.  
-#'
-#' @return the numebr of genomic bins in the hg38 genome
-#'
-"hg38_chr_bins"
